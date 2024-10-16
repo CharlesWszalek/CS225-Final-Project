8b756afa4940789e20347773d053fa3ccd017215
CharlesWszalek <charlesw6203@gmail.com>
2024-10-15 21:58:00 -0700

    making new scratch file

---
+++ FinalProject/scratch1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Charles Wszalek on 10/15/24.
+//
+
+#include "scratch1.h"
---
+++ FinalProject/scratch1.h
@@ -0,0 +1,8 @@
+//
+// Created by Charles Wszalek on 10/15/24.
+//
+
+#ifndef SCRATCH1_H
+#define SCRATCH1_H
+
+#endif //SCRATCH1_H
