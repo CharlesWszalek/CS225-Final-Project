0d0af07da33d4e75558438c9623b141f7b657cf5
ValstraxLS <bpillon21@gmail.com>
2024-11-07 17:21:06 -0700

    cards now return correct value based on new nice looking cards

--- FinalProject/cards.cpp
+++ FinalProject/cards.cpp
@@ -13,31 +13,31 @@ using namespace std;
 CARD::CARD(){}
 
 CARD::CARD(string Suit, string Name) : suit(Suit), name(Name){
-  if(name == "Ace"){
+  if(name == " Ace "){
     value = 0;
-  } else if (name == "2"){
+  } else if (name == "  2  "){
     value = 1;
-  } else if (name == "3"){
+  } else if (name == "  3  "){
     value = 2;
-  } else if (name == "4"){
+  } else if (name == "  4  "){
     value = 3;
-  } else if (name == "5"){
+  } else if (name == "  5  "){
     value = 4;
-  } else if (name == "6"){
+  } else if (name == "  6  "){
     value = 5;
-  } else if (name == "7"){
+  } else if (name == "  7  "){
     value = 6;
-  } else if (name == "8"){
+  } else if (name == "  8  "){
     value = 7;
-  } else if (name == "9"){
+  } else if (name == "  9  "){
     value = 8;
-  } else if (name == "10"){
+  } else if (name == " 10  "){
     value = 9;
-  } else if (name == "Jack"){
+  } else if (name == "Jack "){
     value = 10;
   } else if (name == "Queen"){
     value = 11;
-  } else if (name == "King"){
+  } else if (name == "King "){
     value = 12;
   }
 }
@@ -47,13 +47,13 @@ string CARD::get_suit(){return suit;}
 
 
 int CARD::get_suit_2(){
-  if(suit == "Clubs"){
+  if(suit == " Clubs  "){
     return 0;
   } else if (suit == "Diamonds"){
     return 1;
-  } else if (suit == "Hearts"){
+  } else if (suit == " Hearts "){
     return 2;
-  } else if (suit == "Spades"){
+  } else if (suit == " Spades "){
     return 3;
   }
   return 0;
