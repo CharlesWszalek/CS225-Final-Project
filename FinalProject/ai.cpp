da137285b766f8a342b9481ceb89ecda7e5b8bb5
CharlesWszalek <charlesw6203@gmail.com>
2024-11-12 10:51:48 -0700

    table changes

--- FinalProject/ai.cpp
+++ FinalProject/ai.cpp
@@ -11,21 +11,21 @@
 #include <string>
 using namespace std;
 
-
+// Constructor
 AI::AI(int num, int buyIn, DECK d): PLAYER(num, buyIn, d){srand(time(NULL));name = "Ai " + to_string(num);}
 
-
+// Checks to see if the ai players can bet and then control how much they are allowed to bet
 int AI::bets(){
 	if(hasRaised != -1){
 		int forPot;
-		int chance = rand() % 10;
+		int chance = rand() % 10;  // randomly determine the behavior the the ai
 		if (chance < 4 && hasRaised == 0 && minBet - betMoney + TABLE::bigBlind <= money){
 			forPot = raise();
 		} else if (chance < 8){
 			forPot = check();
 		} else {
 			fold();
-			return -1;
+			return -1; //comunicate player has folded
 		}
 		set_min();
 		return forPot;
@@ -35,17 +35,17 @@ int AI::bets(){
 	}
 }
 
-
+// Checks to see if the ai players can raise and then control how much they are allowed to put into the pot
 int AI::raise(){
 	int betChange;
 	if (money - minBet + betMoney - TABLE::bigBlind == 0){
 		betChange = 5;
 	} else {
-		betChange = rand() % (money - minBet + betMoney - TABLE::bigBlind) + TABLE::bigBlind + 1;
+		betChange = rand() % (money - minBet + betMoney - TABLE::bigBlind) + TABLE::bigBlind + 1;  // randomly determine the behavior the the ai
 	}
 	hasRaised = 1;
 	int moneyForPot = betChange + minBet - betMoney;
-	money -= moneyForPot;
+	money -= moneyForPot; // remove the money from the ai's hand
 	betMoney = minBet + betChange;
 	cout << "raised by " << betChange << " to " << betMoney << endl;
 	return moneyForPot;
