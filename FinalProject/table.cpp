80094849b89f227ab139abc47ce35b4e9b3da6e0
Thomas-Zanin <zanint@my.erau.edu>
2024-11-12 11:27:10 -0700

    File IO in bottom of showdown

--- FinalProject/table.cpp
+++ FinalProject/table.cpp
@@ -453,6 +453,12 @@ void TABLE::showdown(){
 		}
 	}
 	cout << "The winner is: " << players[player_winner]->get_name() << endl;
+	//File IO stuff
+	ofstream myfile;
+	myfile.open("GameHistory.txt");
+	myfile << "The winner of the game is: " << players[player_winner]->get_name() << endl;
+	myfile << "They won " << pot << " dollars!" << endl;
+	myfile.close();
 }
 
 
