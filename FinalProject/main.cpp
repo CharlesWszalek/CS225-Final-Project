dc6f195b4e339542d300cdedb31976207a66d4bf
CharlesWszalek <charlesw6203@gmail.com>
2024-11-12 11:22:50 -0700

    table changes

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -11,39 +11,37 @@
 
 using namespace std;
 
-
+// creates a game for the player
 void game() {
-	int numPlayers;
-	int buyIn;
+	int numPlayers; // number of players playing: 1 + number of ai
+	int buyIn; // the buy in for that game
 
-	cout << "How many players? " << endl;
+	cout << "How many players? " << endl; // read in number of players
 	cin >> numPlayers;
 	cin.ignore(INT_MAX, '\n');
 
-	cout << "What is the buy in? " << endl;
+	cout << "What is the buy in? " << endl; // read in the buy in
 	cin >> buyIn;
 	cin.ignore(INT_MAX, '\n');
 
-	//deck.shuffle();
+	TABLE table(numPlayers, buyIn); // create a table for the game to be played on
 
-	TABLE table(numPlayers, buyIn);
+	table.betting(); //
+	table.flop();    //
+	table.betting(); //
+	table.turn();    // have the players play their turns and add cards to the table
+	table.betting(); //
+	table.river();   //
+	table.betting(); //
 
-	//table.buy_in(buyIn);
+	table.display(); // show the cards on the table and in everyones hands
 
-	table.betting();
-	table.flop();
-	table.betting();
-	table.turn();
-	table.betting();
-	table.river();
-	table.betting();
-	table.display();
-	table.showdown();
+	table.showdown(); // compare the cards of each of the players and determine a winner
 }
 
-
+// main loop to create games
 int main(){
-	string input;
+	string input; // take input yes or no
 	cout << "Shall we play a game? " << endl;
 	cin >> input;
 	while (input == "Yes" || input == "yes")
