c1feb2e51533c7b04698f5a92fb5a33f5ed4a0d0
Charles Wszalek <charlesw6203@gmail.com>
2024-11-13 07:56:14 -0700

    Update global.cpp

--- FinalProject/global.cpp
+++ FinalProject/global.cpp
@@ -3,6 +3,8 @@
 // Name: global.cpp
 // Version 1.0 name: Charles W 11/03/24 created
 // 	   1.1 name: Brandon P 11/05/24 added conv_string_int
+// 	   1.2 name: Thomas Z  10/30/24 added the deck to global so all could access it
+// 	   1.2 name: Brandon P 11/8/24  removed the deck to global for better solution
 // Reference: 
 //
 
