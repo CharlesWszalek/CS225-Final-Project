221854245ccff52ce6f848c841fbdf517e05719f
ValstraxLS <bpillon21@gmail.com>
2024-11-13 10:31:50 -0700

    n

--- FinalProject/deck.hpp
+++ FinalProject/deck.hpp
@@ -1,7 +1,7 @@
 //
 // CSS 225 FINAL PROJECT 
 // Name: deck.h
-// Version 1.0 name: Bradon P  10/22/24 created
+// Version 1.0 name: Brandon P  10/22/24 created
 // Version 1.1 name: Thomas Z  10/22/24 Creating Deck class, function prototypes
 // Version 1.2 name: Charles W 11/06/24 updated to match cpp
 //
