11dcdae9d0679125802b20c68a6900d4d3ab0380
Charles Wszalek <charlesw6203@gmail.com>
2024-11-13 07:57:00 -0700

    Update global.hpp

--- FinalProject/global.hpp
+++ FinalProject/global.hpp
@@ -2,7 +2,9 @@
 // CSS 225 FINAL PROJECT 
 // Name: global.hpp
 // Version 1.0 name: Charles W 11/03/24 created
-// 	   1.1 name: Brandon P 11/05/24 added conv_string_int
+// 	       1.1 name: Brandon P 11/05/24 added conv_string_int
+// 	       1.2 name: Thomas Z  10/30/24 added the deck to global so all could access it
+// 	       1.2 name: Brandon P 11/8/24  removed the deck to global for better solution
 // Reference: 
 //
 
