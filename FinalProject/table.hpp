65d6d376cb9670ed5175100914f8b5f8115da8fb
Charles Wszalek <charlesw6203@gmail.com>
2024-11-13 08:12:47 -0700

    Update table.hpp

--- FinalProject/table.hpp
+++ FinalProject/table.hpp
@@ -1,7 +1,13 @@
 //
 // CSS 225 FINAL PROJECT 
-// Name: table.h
+// Name: table.hpp
 // Version 1.0 name: Bradon P 10/21/24 Changes moved table to seperate header file
+//	   1.1 name: Thomas Z  10/23/24 created new fucntions 
+//	   1.2 name: Charles W 10/27/24 created showdown function
+//	   1.3 name: Charles W 10/30/24 added hand checkers for showdown
+//	   1.4 name: Charles W 11/2/24 added hand checkers for showdown
+//	   1.5 name: Thomas Z  11/3/24 added hand checkers for showdown
+//	   1.6 name: Charles W 11/7/24 added hand checkers for showdown
 // Reference: https://en.wikipedia.org/wiki/Texas_hold_%27em#Play_of_the_hand
 //
 
